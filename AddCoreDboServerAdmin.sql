2323636302fc75f29c8ac4135b9e2d32c0586186
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-10-12 17:00:14 +0200

    make it more flexible to add other users as admin

--- AddCoreDboServerAdmin.sql
+++ AddCoreDboServerAdmin.sql
@@ -1,40 +1,46 @@
--- Add CoreDbo user to SQL Server with full admin rights (same as sa)
--- This script creates the CoreDbo login and grants it sysadmin privileges
+-- Add user to SQL Server with full admin rights (same as sa)
+-- This script creates the user login and grants it sysadmin privileges
 
+-- Set the username here - change this to switch between users
+-- Change 'InstrumentationDbo' to 'CoreDbo' if needed
 USE [master]
-GO
 
--- Create CoreDbo login if it doesn't exist
-IF NOT EXISTS (SELECT name FROM sys.server_principals WHERE name = 'CoreDbo')
+DECLARE @Username NVARCHAR(50) = 'InstrumentationDbo';
+DECLARE @Password NVARCHAR(50) = 'thunderbus888';
+DECLARE @SQL NVARCHAR(MAX);
+
+-- Create login if it doesn't exist
+IF NOT EXISTS (SELECT name FROM sys.server_principals WHERE name = @Username)
 BEGIN
-    CREATE LOGIN [CoreDbo] WITH PASSWORD = 'thunderbus888', 
+    SET @SQL = 'CREATE LOGIN [' + @Username + '] WITH PASSWORD = ''' + @Password + ''', 
                                 DEFAULT_DATABASE=[master], 
                                 CHECK_EXPIRATION=OFF, 
-                                CHECK_POLICY=OFF;
-    PRINT 'CoreDbo login created successfully.'
+                                CHECK_POLICY=OFF;';
+    EXEC sp_executesql @SQL;
+    PRINT @Username + ' login created successfully.'
 END
 ELSE
 BEGIN
     -- Update password if login already exists
-    ALTER LOGIN [CoreDbo] WITH PASSWORD = 'thunderbus888';
-    PRINT 'CoreDbo login already exists - password updated.'
+    SET @SQL = 'ALTER LOGIN [' + @Username + '] WITH PASSWORD = ''' + @Password + ''';';
+    EXEC sp_executesql @SQL;
+    PRINT @Username + ' login already exists - password updated.'
 END
-GO
 
--- Add CoreDbo to sysadmin role (gives full server access like sa)
+-- Add user to sysadmin role (gives full server access like sa)
 IF NOT EXISTS (SELECT 1 FROM sys.server_role_members rm 
                JOIN sys.server_principals r ON rm.role_principal_id = r.principal_id 
                JOIN sys.server_principals m ON rm.member_principal_id = m.principal_id 
-               WHERE r.name = 'sysadmin' AND m.name = 'CoreDbo')
+               WHERE r.name = 'sysadmin' AND m.name = @Username)
 BEGIN
-    ALTER SERVER ROLE [sysadmin] ADD MEMBER [CoreDbo];
-    PRINT 'CoreDbo added to sysadmin role successfully.'
+    SET @SQL = 'ALTER SERVER ROLE [sysadmin] ADD MEMBER [' + @Username + '];';
+    EXEC sp_executesql @SQL;
+    PRINT @Username + ' added to sysadmin role successfully.'
 END
 ELSE
 BEGIN
-    PRINT 'CoreDbo is already a member of sysadmin role.'
+    PRINT @Username + ' is already a member of sysadmin role.'
 END
-GO
 
 -- Verify the setup
 SELECT 
@@ -45,7 +51,7 @@ SELECT
 FROM sys.server_principals sp
 LEFT JOIN sys.server_role_members srm ON sp.principal_id = srm.member_principal_id
 LEFT JOIN sys.server_principals sr ON srm.role_principal_id = sr.principal_id
-WHERE sp.name = 'CoreDbo'
+WHERE sp.name = @Username
 ORDER BY sp.name, sr.name;
 
-PRINT 'CoreDbo setup complete. User now has full server admin rights.'
+PRINT @Username + ' setup complete. User now has full server admin rights.'
